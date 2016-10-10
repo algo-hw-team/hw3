89f7f42343c2cc9360a853a4d81157aebe41e014
Join <choin1116@gmail.com>
2016-10-10 18:39:57 +0900

    Finish

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -11,13 +11,14 @@ import java.util.ArrayList;
 
 public class MainApp {
 
-    private final static String basePath = "c:/hw3/";
+    private final static String basePath = "/Users/Join/dev/homeworks-0302/algo/hw3/";
     private final static String inputPath = basePath + "input.txt";
     private final static String outputPath = basePath + "2013147550.txt";
 
     private static StringBuilder builder = new StringBuilder();
 
     public static void main(String[] args) {
+
         try {
             //모든 인풋 텍스트를 라인단위로 리스트에 저장한다.
             BufferedReader br = new BufferedReader(new FileReader(inputPath));
@@ -70,7 +71,6 @@ public class MainApp {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     private static String runTestCase(int priceToPay,
@@ -78,25 +78,126 @@ public class MainApp {
                        ArrayList<Integer> coinTypes,
                        ArrayList<Integer> numOfCoins) {
 
-        int maxPriceCanPay = 0;
-        int totalCoins = -1;
+        int minTotalNumOfCoins = -1;
+        int minNumOfPaidCoins = -1;
         ArrayList<Integer> minCoinsPaid = null;
         ArrayList<Integer> minCoinsReceived = null;
+        CoinCalculator calculator = new CoinCalculator(coinTypes, numOfCoins);
+        ArrayList<Integer> payablePrices = getPayablePrices(priceToPay,
+                numOfCoinTypes,
+                coinTypes,
+                numOfCoins);
+//        int maxPriceCanPay = getPrice(numOfCoinTypes, coinTypes, numOfCoins);
 
-        for (int i = 0; i < numOfCoinTypes; i++) {
-            maxPriceCanPay += (coinTypes.get(i) * numOfCoins.get(i));
-        }
-
-        for (int price = priceToPay; price <= maxPriceCanPay; price++) {
+        for (int price : payablePrices) {
             // Dynamic Programming start
+
+            // no combination to pay `price`
+            ArrayList<Integer> paidCombination = calculator.findPay(price);
+            if (paidCombination == null) {
+                continue;
+            }
+
+            // no combination to receive changes (`price - priceToPay`)
+            ArrayList<Integer> receivedCombination = calculator.findChanges(price - priceToPay);
+            if (receivedCombination == null) {
+                continue;
+            }
+
+            int numOfPaidCoins = getNumOfCoinsUsed(paidCombination);
+            int numOfReceivedCoins = getNumOfCoinsUsed(receivedCombination);
+            int totalNumOfCoins = numOfPaidCoins + numOfReceivedCoins;
+
+            boolean isFirstCase = minTotalNumOfCoins < 0;
+            boolean hasUsedLesserCoins = totalNumOfCoins < minTotalNumOfCoins;
+            boolean hasUsedSameCoinsAndPaidLess = (minTotalNumOfCoins == totalNumOfCoins) &&
+                    (numOfPaidCoins < minNumOfPaidCoins);
+
+            // if true, current iteration is case that uses minimum coins
+            if (isFirstCase || hasUsedLesserCoins || hasUsedSameCoinsAndPaidLess) {
+                minTotalNumOfCoins = totalNumOfCoins;
+                minNumOfPaidCoins = numOfPaidCoins;
+                minCoinsPaid = paidCombination;
+                minCoinsReceived = receivedCombination;
+            }
         }
 
-        // no output
-        if (totalCoins < 0) {
+        if (minTotalNumOfCoins < 0) {
+            // no output
             return "-1";
         } else {
-            return makeOutputString(totalCoins, minCoinsPaid, minCoinsReceived);
+            return makeOutputString(minTotalNumOfCoins, minCoinsPaid, minCoinsReceived);
         }
+    }
+
+    private static ArrayList<Integer> getPayablePrices(int priceToPay,
+                                                       int numOfCoinTypes,
+                                                       ArrayList<Integer> coinTypes,
+                                                       ArrayList<Integer> numOfCoins) {
+        ArrayList<Integer> payablePrices = new ArrayList<>();
+        int lastIndex = numOfCoinTypes - 1;
+        int availableCoinCombinations = 1;
+        int maxPriceCanPay = 1;
+
+        for (int numOfCoin : numOfCoins) {
+            availableCoinCombinations *= numOfCoin;
+        }
+        for (int i = 0; i < numOfCoinTypes; i++) {
+            maxPriceCanPay += coinTypes.get(i) * numOfCoins.get(i);
+        }
+
+        if ((maxPriceCanPay - priceToPay) <= availableCoinCombinations) {
+            for (int i = priceToPay; i <= maxPriceCanPay; i++) {
+                payablePrices.add(i);
+            }
+
+            return payablePrices;
+        }
+
+
+        ArrayList<Integer> current = new ArrayList<>(numOfCoins);
+
+        while (current.get(lastIndex) >= 0) {
+            int price = getPrice(numOfCoinTypes, coinTypes, current);
+
+            // adds payable price only when it is bigger than priceToPay
+            if (price >= priceToPay) {
+                payablePrices.add(price);
+            }
+
+            // calculate next available combination
+            current.set(0, current.get(0) - 1);
+            for (int i = 0; i < numOfCoinTypes - 1; i++) {
+                if (current.get(i) < 0) {
+                    current.set(i, numOfCoins.get(i));
+                    current.set(i + 1, current.get(i + 1) - 1);
+                }
+            }
+        }
+
+        return payablePrices;
+    }
+
+    private static int getPrice(int numOfCoinTypes,
+                                ArrayList<Integer> coinTypes,
+                                ArrayList<Integer> combination) {
+        int result = 0;
+
+        for (int i = 0; i < numOfCoinTypes; i++) {
+            result += (coinTypes.get(i) * combination.get(i));
+        }
+
+        return result;
+    }
+
+    private static int getNumOfCoinsUsed(ArrayList<Integer> combinations) {
+        int totalCoins = 0;
+
+        for (int coinUsed : combinations) {
+            totalCoins += coinUsed;
+        }
+
+        return totalCoins;
     }
 
     private static String makeOutputString(int totalCoins,
